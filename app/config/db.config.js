5d550affe01994e93b1bf0992d314d0bce810708
egorzeleznov <egorshelznov@gmail.com>
2021-09-29 16:44:19 +0300

    new API

---
+++ app/config/db.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    HOST: "postiki",
+    PASS: mwcFlzj4S3rnfGZN,
+    DB: "postiki"
+};
+//mongodb+srv://postiki:mwcFlzj4S3rnfGZN@cluster0.lkbdg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
---
+++ server.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const db = require("./app/models");
+
+const Role = db.role;
+
+const app = express();
+
+const corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
+db.mongoose
+    .connect("mongodb+srv://postiki:33ee2w1q@cluster0.lkbdg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Successfully connect to MongoDB.");
+        initial();
+    })
+    .catch(err => {
+        console.error("Connection error", err);
+        process.exit();
+    });
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// simple route
+app.get("/", (req, res) => {
+    res.json({message: "Welcome to postiki application."});
+});
+
+function initial() {
+    Role.estimatedDocumentCount((err, count) => {
+        if (!err && count === 0) {
+            new Role({
+                name: "user"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+
+                console.log("added 'user' to roles collection");
+            });
+
+            new Role({
+                name: "moderator"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+
+                console.log("added 'moderator' to roles collection");
+            });
+
+            new Role({
+                name: "admin"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+
+                console.log("added 'admin' to roles collection");
+            });
+        }
+    });
+}
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
